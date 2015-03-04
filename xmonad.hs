e9eaaa8d70cd76f94a3d10be1bc840ca136dc231
Dave Benvenuti <davebenvenuti@gmail.com>
2015-03-04 13:48:27 -0500

    added another source, got volume control

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,15 @@
 -- Pretty good resources:
 --   http://beginners-guide-to-xmonad.readthedocs.org/en/latest/configure_xmonadhs.html
+--   http://iaindunning.com/2013/experiences-with-xmonad-and-ubuntu-12.html
 --   https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration
+--   https://github.com/Emantor/configs/blob/master/xmonad/xmonad.hs
 
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import Graphics.X11.ExtraTypes.XF86
 import System.IO
 import XMonad.Actions.CycleWS
 
@@ -29,6 +32,8 @@ main = do
     } `additionalKeys`
     [ ((mod1Mask .|. controlMask, xK_Right), nextWS)
     , ((mod1Mask .|. controlMask, xK_Left), prevWS)
-    , ((mod1Mask .|. controlMask, xK_l), spawn "xscreensaver-command -lock; xset dpms force off")
+    , ((mod1Mask .|. controlMask, xK_l), spawn "gnome-screensaver-command -lock")
+    , ((0 , xF86XK_AudioLowerVolume), spawn "~/.xmonad-pulsevolume/pulse-volume.sh decrease")
+    , ((0 , xF86XK_AudioRaiseVolume), spawn "~/.xmonad-pulsevolume/pulse-volume.sh increase")
     ]
 
