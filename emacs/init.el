00d4a25bdec23ec3c8b1892eee127bb11aafefbd
Dave Benvenuti <davebenvenuti@gmail.com>
2016-12-23 14:48:50 -0800

    update package repos

--- emacs/init.el
+++ emacs/init.el
@@ -31,7 +31,13 @@
 ;; el-get ELPA stuff
 (require 'package)
 (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/el-get")
 (require 'el-get)
 
