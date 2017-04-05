e55e19b21ee0a98a2d0b0be9bf00738961cc0212
Dave Benvenuti <davebenvenuti@gmail.com>
2017-04-05 08:56:20 -0700

    melpa

--- emacs/init.el
+++ emacs/init.el
@@ -40,6 +40,17 @@
     (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+          ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+
 (add-to-list 'load-path "~/.emacs.d/el-get")
 (require 'el-get)
 
